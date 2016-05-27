373e2c1c87f252e054ade61f2801d8dba8fa9320
Kevin Dangoor <kevindangoor@khanacademy.org>
2016-05-27 14:39:04 -0400

    Pull Subdivide from my fork (which now has typings) in order to properly use it.

--- interviewer/index.tsx
+++ interviewer/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {createStore, combineReducers} from "redux";
 
 import Subdivide from "subdivide";
 
--- thirdparty/subdivide.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-interface SubdivideProps {
-    DefaultComponent: {new(...args: any[]): React.Component<any, any>}
-}
