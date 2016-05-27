7b2ab3695a45261fc6b5f6337fa07030bb7f9adb
Kevin Dangoor <kevindangoor@khanacademy.org>
2016-05-27 10:10:26 -0400

    Commit basic scaffolding to do client side code loading and use Subdivide.

---
+++ interviewer/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import Subdivide from "subdivide";
+
+import {Hello} from "./components/hello";
+
+let hello = <Hello compiler="TypeScript" framework="React" />; 
+
+ReactDOM.render(
+    <Subdivide DefaultComponent={Hello}/>,
+    document.getElementById("container")
+);
---
+++ thirdparty/subdivide.d.ts
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+interface SubdivideProps {
+    DefaultComponent: {new(...args: any[]): React.Component<any, any>}
+}
